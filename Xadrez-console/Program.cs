8f1791b7fd544e437161818a5856962d9d633da5
Gustavo Figueiroa <gustavofsoledade@hotmail.com>
2022-11-25 18:19:05 -0300

    Começando a colocar peças no tabuleiro

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,6 +1,12 @@
-﻿
-using tabuleiro;
+﻿using tabuleiro;
 using Xadrez_console;
+using xadrez;
 
-Tabuleiro tab = new Tabuleiro(10,10);
+
+Tabuleiro tab = new Tabuleiro(8,8);
+
+tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
+tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+
 Tela.imprimirTabuleiro(tab);
